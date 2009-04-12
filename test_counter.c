bd25abfa627ea81f98e699fc9767deda7f2f171b
Evan Klitzke <evan@eklitzke.org>
2009-04-12 11:43:43 -0700

    a threaded test

--- test_counter.c
+++ test_counter.c
@@ -1,7 +1,10 @@
 #include "scribl.h"
 #include <stdio.h>
 
-int main()
+gpointer thread_worker(gpointer data);
+
+/* Test using a counter without threads */
+void test_simple()
 {
 	struct scribl_counter *counter;
 	
@@ -14,5 +17,42 @@ int main()
 	printf("foo.baz = %d\n", scribl_lookup_counter(counter, "foo", "baz"));
 	printf("foo.bad = %d\n", scribl_lookup_counter(counter, "foo", "bad"));
 	scribl_free_counter(counter);
+}
+
+/* Test using a counter with threads */
+void test_two_threads()
+{
+	struct scribl_counter *counter;
+	guint *val;
+	GThread *thd1, *thd2;
+
+	scribl_init();
+	counter = scribl_new_counter();
+	thd1 = g_thread_create(thread_worker, counter, TRUE, NULL);
+	thd2 = g_thread_create(thread_worker, counter, TRUE, NULL);
+	val = g_thread_join(thd1);
+	printf("thd1 exited with val = %d\n", *val);
+	val = g_thread_join(thd2);
+	printf("thd2 exited with val = %d\n", *val);
+}
+
+gpointer thread_worker(gpointer data)
+{
+	int i;
+	guint *val;
+	struct scribl_counter *counter;
+
+	counter = (struct scribl_counter *) data;
+	for (i = 0; i < 500000; i++) {
+		scribl_incr_counter(counter, "foo", "bar");
+	}
+	val = g_slice_alloc(sizeof(guint));
+	*val = scribl_lookup_counter(counter, "foo", "bar");
+	g_thread_exit(val);
+}
+
+int main()
+{
+	test_two_threads();
 	return 0;
 }
